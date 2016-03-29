3e0ff92275d782f34ae249be78bdf65fd2558740
Isaac Wang <icw001@ucsd.edu>
2016-03-29 14:06:43 -0700

    Added Game.java to the project

---
+++ stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/Game.java
@@ -0,0 +1,90 @@
+package com.example.brian.stazoresistance;
+
+/**
+ * Created by Brian on 3/29/2016.
+ */
+public class GameObject {
+
+    private static final int win = 3; //How many rounds it takes to win the game
+
+    private static int playerCount; //How many players are in the game
+
+    private String[] players; //Array of player names
+    private Boolean[] spy; //Parallel array of t/f depending on if the player is a spy or not
+    private int playerIndex = 0; //Where to insert the next player
+
+    private int spyScore = 0; //Score of the Spies
+    private int resistanceScore = 0; //Score of the Resistance
+
+    public GameObject (int count) {
+        playerCount = count;
+
+        players = new String[playerCount];
+        spy = new Boolean[playerCount];
+
+        randomizeSpies();
+    }
+
+    /**
+     * Assigns the spies randomly in the spy array.
+     */
+    private void randomizeSpies () {
+        int maxSpies = (playerCount + 2) / 3; //Gets max number of spies according to playerCount
+        int numSpies = 0; //Current number of spies in array
+
+        for (int i = 0; i < spy.length; i++) {
+
+            spy[i] = false;
+
+            //Already assigned all the spies, keep filling up the rest with false.
+            if (numSpies >= maxSpies) {
+                continue;
+            }
+
+            //Randomize spies; 0 = not spy, 1 = spy
+            int randomNum = (int)(Math.random());
+
+            if (randomNum == 1) {
+                spy[i] = true;
+                numSpies++;
+            }
+        }
+
+        //Failed to assign correct number of spies, redo
+        if (numSpies < maxSpies) {
+            randomizeSpies();
+        }
+    }
+
+    public int getNumPlayers () {
+        return playerCount;
+    }
+
+    /**
+     * Add a player to the array.
+     * @param player The player to be added.
+     * @return t/f depending on success of adding the player.
+     */
+    public Boolean addPlayer (String player) {
+
+        if (playerIndex < playerCount) {
+
+            players[playerIndex] = player;
+            playerIndex++;
+
+        }
+        else {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int getSpyScore () {
+        return spyScore;
+    }
+
+    public int getResistanceScore () {
+        return resistanceScore;
+    }
+}
