a883037f2a20a8c2e16af56d8622fec714a40237
Luke Thomas <lukethethomas@gmail.com>
2016-03-29 20:11:39 -0700

    My changes

---
+++ stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/RoleRevealActivity.java
@@ -0,0 +1,15 @@
+package com.example.isaacwang.stazo_resistance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoleRevealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rolereveal);
+        Intent intent = getIntent();
+    }
+}
