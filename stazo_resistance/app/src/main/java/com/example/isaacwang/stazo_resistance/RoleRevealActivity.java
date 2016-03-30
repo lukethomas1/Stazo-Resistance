b6285d0267cd77caa22549164d2fbc1d118112ca
Luke Thomas <lukethethomas@gmail.com>
2016-03-29 20:57:00 -0700

    First version of RoleReveal

--- stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/RoleRevealActivity.java
+++ stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/RoleRevealActivity.java
@@ -3,6 +3,8 @@ package com.example.isaacwang.stazo_resistance;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RoleRevealActivity extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class RoleRevealActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rolereveal);
+        // Get the intent that created this activity
         Intent intent = getIntent();
+        // Get the text field that displays the role of the player
+        TextView roleText = (TextView)findViewById(R.id.role_textView);
+        // Set the text to the player's role
+        roleText.setText(intent.getStringExtra("Role"));
     }
+
+    public void goToName(){
+        Intent i = new Intent(this, NameEntry.class);
+        startActivity(i);
+    };
 }
