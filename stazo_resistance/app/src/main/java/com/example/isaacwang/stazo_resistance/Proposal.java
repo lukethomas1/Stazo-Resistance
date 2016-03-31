b4353215731f4c9a942ebd519f12c18763fd7cb4
Brian Chan <alto1020@gmail.com>
2016-03-30 19:41:08 -0700

    Added the back button functionality

--- stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/Proposal.java
+++ stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/Proposal.java
@@ -1,5 +1,7 @@
 package com.example.isaacwang.stazo_resistance;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -146,5 +148,27 @@ public class Proposal extends AppCompatActivity{
             Intent i = new Intent(this, Approval.class);
             startActivity(i);
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        String[] quitArray = {"Yes", "No"};
+        AlertDialog.Builder numEntry = new AlertDialog.Builder(this);
+        numEntry.setTitle("Are you sure you want to quit this game?");
+        numEntry.setItems(quitArray, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                if (whichButton == 1)
+                    dialog.cancel();
+                else
+                    goToIntro();
+            }
+        });
+        numEntry.create();
+        numEntry.show();
+    }
+
+    public void goToIntro() {
+        Intent i = new Intent(this, IntroActivity.class);
+        startActivity(i);
     }
 }
