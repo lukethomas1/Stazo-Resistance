ab6654ea9c29fa3743e2114bcbfd13354d6bb188
ericzhang98 <zhangeric99@gmail.com>
2016-03-30 19:45:50 -0700

    Edited Game, finished MissionPass

--- stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/MissionPassTho.java
+++ stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/MissionPassTho.java
@@ -5,32 +5,52 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by ericzhang on 3/29/16.
  */
 public class MissionPassTho extends AppCompatActivity{
+    private Game game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mission_pass_tho);
+        Resistance resistance = ((Resistance) this.getApplication());
+        game = resistance.getGame();
+
+        //for updating the text message
+        //if mission passes
+        if (game.executeMission()) {
+            ((TextView) findViewById(R.id.missionPassedText)).setText("Mission Passed! :D");
+
+        }
+        //if mission fails
+        else {
+            ((TextView) findViewById(R.id.missionPassedText)).setText("Mission Failed! D:");
+        }
     }
 
     public void handleClick(View view)
     {
-        //increment resistance points
-        Game game = new Game(7);
-        game.incrementResistanceScore();
-
-        //Just move on to next proposal if resistance points < 3
-        if (game.getResistanceScore() <3) { //heart tho
-//            Intent intent = new Intent(this, Proposal.class);
-//            startActivity(intent);
+        //Resistance won
+        if (game.getResistanceScore() == 3) {
+            //update the fact that resistance won
+            game.setWhoWon(true);
+            Intent intent = new Intent(this, GameOver.class);
+            startActivity(intent);
         }
-        //otherwise gg resistance op, spies suck
+        //Spies won
+        else if (game.getSpyScore() == 3) {
+            //update the fact that spies won
+            game.setWhoWon(false);
+            Intent intent = new Intent(this, GameOver.class);
+            startActivity(intent);
+        }
+        //otherwise go back to proposal screen
         else {
-            //Intent intent = new Intent(this, GameOver.class);
-            //startActivity(intent);
+            Intent intent = new Intent(this, Proposal.class);
+            startActivity(intent);
         }
     }
 }
