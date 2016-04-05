058e971318d443a52e6e60108b3db25a776f1aa9
Isaac Wang <isaacwang4076@gmail.com>
2016-04-04 23:33:08 -0700

    Fixed return to main menu button for game over screen

--- stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/GameOver.java
+++ stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/GameOver.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameOver extends AppCompatActivity {
 
@@ -33,6 +34,11 @@ public class GameOver extends AppCompatActivity {
     }
 
     public void goToIntro() {
+        Intent i = new Intent(this, IntroActivity.class);
+        startActivity(i);
+    }
+
+    public void returnToMain(View view) {
         Intent i = new Intent(this, IntroActivity.class);
         startActivity(i);
     }
