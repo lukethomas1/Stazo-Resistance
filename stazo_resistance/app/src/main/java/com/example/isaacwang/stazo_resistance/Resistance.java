2aba9f41743b273c19bc98b7e8f52af5daf8dacc
ericzhang98 <zhangeric99@gmail.com>
2016-04-09 17:57:59 -0700

    added votemission, edited intro and resistance

--- stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/Resistance.java
+++ stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/Resistance.java
@@ -10,16 +10,18 @@ import com.firebase.client.Firebase;
 public class Resistance extends Application {
     private Game game;
     private static final String fbURL = "https://amber-torch-3377.firebaseio.com/";
-
+    private Player player;
 
     public Game getGame() {
         return game;
     }
     public String getFbURL() { return fbURL;}
+    public Player getPlayer() {return player;}
 
     public void setGame(Game game) {
         this.game = game;
     }
+    public void setPlayer(Player p) {this.player = p;}
 
     @Override
     public void onCreate() {
