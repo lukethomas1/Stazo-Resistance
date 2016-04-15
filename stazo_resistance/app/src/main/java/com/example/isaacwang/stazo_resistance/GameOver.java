e1b3fecd3335222271b03a31810b0d110b8fbecd
Matthew Ung <matthew.ung814@gmail.com>
2016-04-15 02:38:37 -0700

    finished game over!

--- stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/GameOver.java
+++ stazo_resistance/app/src/main/java/com/example/isaacwang/stazo_resistance/GameOver.java
@@ -3,9 +3,11 @@ package com.example.isaacwang.stazo_resistance;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
@@ -14,6 +16,9 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.GenericTypeIndicator;
 import com.firebase.client.ValueEventListener;
 import com.firebase.client.DataSnapshot;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,6 +32,11 @@ public class GameOver extends AppCompatActivity {
     private String winner;
     private String game_id;
     private ArrayList<Player> playerArray;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +50,20 @@ public class GameOver extends AppCompatActivity {
 
 
         //if resistance wins
-        if(winner.equals("Resistance")){
+        if (winner.equals("Resistance")) {
             ((TextView) findViewById(R.id.wonText)).setText("Resistance wins!");
             iteratePlayers(winner);
-        //spies win
+            //spies win
         } else {
             ((TextView) findViewById(R.id.wonText)).setText("Spies win!");
             iteratePlayers(winner);
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void getArray(){
+    private void getArray() {
 
         playerRef.addListenerForSingleValueEvent(new ValueEventListener() {
 
@@ -74,11 +87,15 @@ public class GameOver extends AppCompatActivity {
      * iterates through list of players and
      * dynamically creates textviews for players who have won.
      */
-    private void iteratePlayers(String str){
+    private void iteratePlayers(String str) {
         getArray();
+        LinearLayout layout = (LinearLayout) findViewById(R.id.playersList);
         //iterate through array from firebase
-        for (Player player : playerArray){
-            //dynamically create texviews to be shown...
+        for (Player player : playerArray) {
+            //playersList
+            TextView name = new TextView(this);
+            name.setText(player.getName());
+            layout.addView(name);
         }
     }
 
@@ -104,4 +121,43 @@ public class GameOver extends AppCompatActivity {
         startActivity(i);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "GameOver Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.isaacwang.stazo_resistance/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "GameOver Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.isaacwang.stazo_resistance/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
